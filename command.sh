93c0a8d95383b575c83b792423f60d504bdc8fe7
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2025-04-13 00:12:58 +0530

    Complete first task

---
+++ command.sh
@@ -0,0 +1,13 @@
+# npx create-react-app blackbird
+# cd propeller-app
+# git add .
+# git branch update_logo
+# git checkout main
+# git add .
+# gH repo create [blackbird] –public
+# code .
+# git add .
+# git commit -m “Complete first task”
+# gh pr create –base main
+# gh pr merge
+# REPO_URL https://github.com/raeplusplus/-blackbird-propeller-task-
